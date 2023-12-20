b985af40a21d7b127369b2e27e4e9c9c523b6482
diego <diego-cristian@live.com.ar>
2023-12-20 05:58:47 -0300

    404

--- 404.php
+++ 404.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./css/estilo.css">
-    <title>Error404</title>  
-</head>
-<body class="error">    
-    <div class="mainbox">
-        <div class="err">4</div>
-        <i class="far fa-question-circle fa-spin"></i>
-        <div class="err2">4</div>
-        <div class="msg">No te quedes sin tus frutos<p>Vayamos a <a href="../pages/contacto.html">CONTACTO</a> e intentemos desde allí.</p></div>
-    </div>   
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./css/estilo.css">
+        <title>Error404</title>  
+    </head>
+    <body class="error">    
+        <div class="mainbox">
+            <div class="err">4</div>
+            <i class="far fa-question-circle fa-spin"></i>
+            <div class="err2">4</div>
+            <div class="msg">No te quedes sin tus frutos<p>Vayamos a <a href="../pages/contacto.html">CONTACTO</a> e intentemos desde allí.</p></div>
+        </div>   
+    </body>
 </html>
